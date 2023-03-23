6755818a2dba30c94ec01f93106aa49e3cfd99ae
naikymen <you@example.com>
2023-03-23 20:10:53 -0300

    note unimplemented actions

--- pylabrobot/liquid_handling/backends/klipper_backend.py
+++ pylabrobot/liquid_handling/backends/klipper_backend.py
@@ -3,14 +3,24 @@ Klipper backend for PLR.
 
 It relies on the websocket API provided by the Moonkraker program.
 
+From CONTRIBUTING.md:
+
+> Writing a new backend
+>
+> Backends are the primary objects used to communicate with hardware. If you want to integrate a new piece of hardware into PyLabRobot, writing a new backend is the way to go. Here's, very generally, how you'd do it:
+>
+> 1. Copy the `pylabrobot/liquid_handling/backends/backend.py` file to a new file, and rename the class to `<BackendName>Backend`.
+> 2. Remove all `abc` (abstract base class) imports and decorators from the class.
+> 3. Implement the methods in the class.
+
 Options:
 
-- subclass LiquidHandlerBackend: "Abstract base class for liquid handling robot backends".
+- subclass LiquidHandlerBackend: "Abstract base class for liquid handling robot backends", as suggested in CONTRIBUTING.md.
 - subclass SerializingBackend: "A backend that serializes all commands received, and sends them to `self.send_command` for
   processing. The implementation of `send_command` is left to the subclasses.".
 
-SerializingBackend makes more sense as it has the serializing feature, it is also a subclass of LiquidHandlerBackend.
-The chatterbox and opentrons backends use only LiquidHandlerBackend. Sticking to it then...
+SerializingBackend makes more sense as it has the serializing feature; it is also a subclass of LiquidHandlerBackend.
+The chatterbox and opentrons backends use only LiquidHandlerBackend. Sticking to that for now...
 
 To-do:
 
@@ -63,6 +73,7 @@ class ChatterBoxBackend(LiquidHandlerBackend):
     async def unassigned_resource_callback(self, name: str):
         print(f"Resource {name} was unassigned from the robot.")
 
+    # Atomic implemented in hardware.
     async def pick_up_tips(self, ops: List[Pickup], use_channels: List[int], **backend_kwargs):
         print(f"Picking up tips {ops}.")
 
@@ -75,17 +86,20 @@ class ChatterBoxBackend(LiquidHandlerBackend):
     async def dispense(self, ops: List[Dispense], use_channels: List[int], **backend_kwargs):
         print(f"Dispensing {ops}.")
 
-    async def move_resource(self, move: Move, **backend_kwargs):
-        print(f"Moving {move}.")
+    # Atomic actions not implemented in hardware.
+    # TODO: implement these methods as a required human intervention.
+    async def pick_up_tips96(self, pickup: PickupTipRack):
+        raise NotImplementedError("The backend does not support the CoRe 96.")
 
-    async def pick_up_tips96(self, pickup: PickupTipRack, **backend_kwargs):
-        print(f"Picking up tips from {pickup.resource.name}.")
-
-    async def drop_tips96(self, drop: DropTipRack, **backend_kwargs):
-        print(f"Dropping tips to {drop.resource.name}.")
+    async def drop_tips96(self, drop: DropTipRack):
+        raise NotImplementedError("The backend does not support the CoRe 96.")
 
     async def aspirate96(self, aspiration: AspirationPlate):
-        print(f"Aspirating {aspiration.volume} from {aspiration.resource}.")
+        raise NotImplementedError("The backend does not support the CoRe 96.")
 
     async def dispense96(self, dispense: DispensePlate):
-        print(f"Dispensing {dispense.volume} to {dispense.resource}.")
+        raise NotImplementedError("The backend does not support the CoRe 96.")
+
+    async def move_resource(self, move: Move):
+        """ Move the specified lid within the robot. """
+        raise NotImplementedError("Moving resources is not implemented yet.")
