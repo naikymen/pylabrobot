e76b28023de024b84952e17c4bfc842a551c7f58
naikymen <you@example.com>
2023-08-06 20:16:07 -0300

    fix: pass arguments to _setup* functions

--- pylabrobot/liquid_handling/backends/piper_backend.py
+++ pylabrobot/liquid_handling/backends/piper_backend.py
@@ -46,12 +46,12 @@ class PiperBackend(LiquidHandlerBackend):
                  ws_address: str = "ws://localhost:7125/websocket", # Moonraker server.
                  verbose: bool = True,
                  protocol_name=None,
-                 protocol=None, 
+                 protocol=None,
                  workspace=None,
                  platforms_in_workspace=None,
                  dry=False):
         """Init method for the PiperBackend.
-        
+
         Note: 'protocol_name' will override the other parameters if a MongoDB address is given.
         """
 
@@ -76,11 +76,11 @@ class PiperBackend(LiquidHandlerBackend):
 
         # Get workspace info (i.e. the "deck" in PLR terms) from MongoDB if up.
         if self.mo and protocol_name:
-            
+
             # if not protocol_name:
             #     # Get the name of the newest protocol by default.
             #     protocol_name = self.protocols[-1]["name"]
-            
+
             # Get the main objects from the protocol's name.
             protocol, workspace, platforms_in_workspace = self.mo.getProtocolObjects(protocol_name=protocol_name)
             # Other options:
@@ -90,7 +90,7 @@ class PiperBackend(LiquidHandlerBackend):
 
         # Save objects
         self.protocol, self.workspace, self.platforms_in_workspace = protocol, workspace, platforms_in_workspace
-        
+
         # Instatiate the GCODE builder class
         self.builder = GcodeBuilder(protocol=protocol,
                                     workspace=workspace,
@@ -113,16 +113,16 @@ class PiperBackend(LiquidHandlerBackend):
         await super().setup()
 
         # Connect to Moonraker.
-        await self._setup_moonraker()
+        await self._setup_moonraker(timeout, reset_firmware_on_error)
 
         # Home the robot.
-        await self._home_machine()
+        await self._home_machine(timeout)
 
-    async def _setup_moonraker(self):
+    async def _setup_moonraker(self, timeout, reset_firmware_on_error):
         if self.dry:
             print("Dry mode enabled, skipping Moonraker connection.")
             return
-        
+
         print("Setting up connection to the robot...")
         self.moon.start_as_task()
         ws_ready = await self.moon.wait_for_setup(timeout=timeout, raise_error=True)
@@ -133,8 +133,8 @@ class PiperBackend(LiquidHandlerBackend):
             raise Exception("Klipper is not ready, check its logs.")
         else:
             print("Connections successfully setup.")
-    
-    async def _home_machine(self):
+
+    async def _home_machine(self, timeout):
         if self.dry:
             print("Dry mode enabled, skipping Homing routine.")
             return
@@ -166,17 +166,17 @@ class PiperBackend(LiquidHandlerBackend):
             return
         else:
             print("Stopping the robot.")
-            
+
             # Try sending a "motors-off" command.
             cmd_id = await self.moon.send_gcode_cmd("M84", wait=True, check=True, timeout=timeout)
-            
+
             # Send an "emergency stop" command if M84 failed.
             if not await self.moon.check_command_result_ok(cmd_id=cmd_id, timeout=timeout, loop_delay=0.2):
                 self.moon.firmware_restart()
-            
+
             # TODO: comment on what this does.
             await super().stop()
-            
+
             # Close the connection to moonraker.
             await self.moon.stop()
 
@@ -195,14 +195,14 @@ class PiperBackend(LiquidHandlerBackend):
     @property
     def num_channels(self) -> int:
         return self._num_channels
-    
+
     @staticmethod
     def get_coords(operation):
         # pick_up_op
         coordinate = operation.get_absolute_location()
         coordinate_dict = coordinate.serialize()
         return coordinate_dict
-    
+
     async def _send_command_wait_and_check(self, gcode, timeout):
         # TODO: customize timeout.
         # Send gcode commands.
@@ -214,7 +214,7 @@ class PiperBackend(LiquidHandlerBackend):
             return await self.moon.wait_for_idle_printer(timeout=1.0)
         else:
             print(f"Backend in dry mode, commands ignored:\n{pformat(gcode)}")
-    
+
     # Pipetting action generators ####
 
     def make_home_action(self, axes=None):
@@ -222,7 +222,7 @@ class PiperBackend(LiquidHandlerBackend):
         if axes:
             home_action["args"] = {"axis": axes}
         return home_action
-    
+
     def make_tip_pickup_action(self, operation: Pickup, tool_id: str):
         # TODO: Add validation through "jsonschema.validate" (maybe in piper, maybe here).
         # NOTE: The platform version of the content definition is not useful for now.
@@ -232,12 +232,12 @@ class PiperBackend(LiquidHandlerBackend):
         # NOTE: Using the platformless content definition.
         action = {
             'cmd': 'PICK_TIP',
-            'args': {'coords': self.get_coords(operation), 
+            'args': {'coords': self.get_coords(operation),
                      'tool': tool_id,
                      'tip': {'maxVolume': operation.tip.maximal_volume,
                              'tipLength': operation.tip.total_tip_length,
                              'volume': 0}}}
-        
+
         return action
 
     def make_discard_tip_action(self, operation: Pickup, tool_id: str = None):
@@ -270,9 +270,9 @@ class PiperBackend(LiquidHandlerBackend):
 
     async def pick_up_tips(self, ops: List[Pickup], use_channels: List[int], **backend_kwargs):
         """_summary_
-        
+
         The action can also provide the tip's coordinates directly, but must provide "tip" data explicitly:
-            'args': {'coords': {"x": 20, "y": 200, "z": 30}, 
+            'args': {'coords': {"x": 20, "y": 200, "z": 30},
                     'tool': 'P200',
                     'tip': tip_definition},  # Note the tip data passed here (described below).
             'cmd': 'PICK_TIP'
@@ -290,10 +290,10 @@ class PiperBackend(LiquidHandlerBackend):
 
         # TODO: Handle multi-channel pick-up operations.
         action = self.make_tip_pickup_action(ops[0], tool_id)
-        
+
         # Make GCODE
         gcode = self.builder.parseAction(action=action)
-        
+
         # TODO: have the timeout based on an estimation.
         timeout = 10.0
 
@@ -314,7 +314,7 @@ class PiperBackend(LiquidHandlerBackend):
 
         # Make GCODE
         gcode = self.builder.parseAction(action=drop_tip_action)
-        
+
         # TODO: have the timeout based on an estimation.
         timeout = 10.0
 
@@ -330,10 +330,10 @@ class PiperBackend(LiquidHandlerBackend):
 
         # Make action
         action = self.make_pipetting_action(ops[0], tool_id)
-        
+
         # Make GCODE
         gcode = self.builder.parseAction(action=action)
-        
+
         # TODO: have the timeout based on an estimation.
         timeout = 10.0
 
@@ -346,12 +346,12 @@ class PiperBackend(LiquidHandlerBackend):
         # TODO: Ask Rick how to choose a pipette (or channel from a multichanel pipette).
         #       The OT module has a "select_tip_pipette" method.
         tool_id = None
-        
+
         # Make action:
         # dispense_liquid_action = {
         #     'args': {'item': '5x16_1.5_rack 1', 'selector': {'by': 'name', 'value': 'tube2'}, 'volume': 100},
         #     'cmd': 'DROP_LIQUID'}
-        
+
         # Make action
         # TODO: ask Rick if the "Dispense" operation is really the same as "Aspirate" (see standard.py).
         action = self.make_pipetting_action(ops[0], tool_id)
