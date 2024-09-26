42e15889dfbf0bc14cced19eef5d6f00f649f762
naikymen <you@example.com>
2024-09-26 00:18:58 -0300

    Tip: add active_z, and serialize model, active_z, max_volume (redundant), size_z (redundant)

--- pylabrobot/resources/tip.py
+++ pylabrobot/resources/tip.py
@@ -22,6 +22,7 @@ class Tip:
   maximal_volume: float
   fitting_depth: float
   model: str = "default"
+  active_z: float = 0.0
 
   def __post_init__(self):
     self.tracker = VolumeTracker(max_volume=self.maximal_volume)
@@ -35,7 +36,12 @@ class Tip:
       "fitting_depth": self.fitting_depth,
       # TODO: Discuss this with Rick. The tip may be one of several brands/models.
       #       I need this to know which tip to insert in the rack.
-      "model": self.model
+      "model": self.model,
+      "active_z": self.active_z, # Used for compatible tip tracking of "containerOffsetZ".
+      # TODO: Deduplicate these keys.
+      #       Added it with another key for consistency with Tubes.
+      "max_volume": self.maximal_volume,
+      "size_z": self.total_tip_length,
     }
 
 
