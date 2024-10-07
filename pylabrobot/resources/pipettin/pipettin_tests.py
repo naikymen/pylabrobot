546ab9e4c7159ab02a2b833aea37a3a27b683a71
naikymen <you@example.com>
2024-10-07 14:47:11 -0300

    pipettin_tests: cleanup test_piper_backend, and pass db object to make_silver_deck

--- pylabrobot/resources/pipettin/pipettin_tests.py
+++ pylabrobot/resources/pipettin/pipettin_tests.py
@@ -26,9 +26,10 @@ from newt.translators.utils import calculate_plr_grid_parameters, derive_grid_pa
 # Example using exported data.
 db_location = 'https://gitlab.com/pipettin-bot/pipettin-gui/-/raw/develop/api/src/db/defaults/databases.json'
 
-def make_silver_deck(workspace_name = "MK3 Baseplate"):
+def make_silver_deck(workspace_name = "MK3 Baseplate", db: dict = None):
   print("Deck setup")
-  db = load_objects(db_location)["pipettin"]
+  if db is None:
+    db = load_objects(db_location)["pipettin"]
 
   # Choose one workspace.
   workspace = next(w for w in db["workspaces"] if w["name"] == workspace_name)
@@ -62,38 +63,36 @@ def test_silver_deck():
 @pytest.mark.asyncio
 async def test_piper_backend():
 
-  # Get tool definitions.
-  db = load_objects(db_location)["pipettin"]
-  tool_defs = db["tools"]
-
   # Load default configuration, and set it to dry mode.
   config = default_config()
   config.update({
     "dry": True,
     "ws_address": "",
-    "sio_address": ""
+    "sio_address": "",
+    # Database connection parameters.
+    "datatools": "jsondb",
+    "database": {"json_url": db_location, "database_name": 'pipettin'}
   })
+
   # Piper backend.
-  print("Backend setup")
   back = PiperBackend(config=config)
-  print("Backend setup done")
+
+  # Get object definitions.
+  db = load_objects(db_location)["pipettin"]
 
   # Instantiate the deck.
-  deck = make_silver_deck()
+  deck = make_silver_deck(db=db)
 
   # TODO: Ask for a better error message when a non-instantiated backend is passed.
-  print("LH setup")
   lh = LiquidHandler(backend=back, deck=deck)
   await lh.setup()
-  print("LH done")
 
-  # We enable tip and volume tracking globally using the `set_volume_tracking` and `set_tip_tracking` methods.
-  print("Setting volume tracking")
+  # We enable tip and volume tracking globally using the
+  # `set_volume_tracking` and `set_tip_tracking` methods.
   set_volume_tracking(enabled=True)
   set_tip_tracking(enabled=True)
 
   # Pickup.
-  print("Pickup operation")
   tiprack = lh.get_resource("Blue tip rack")
   tip_spots = tiprack["A1:A2"]
   # TODO: Ask for a better error message when tips are passed instead of tip spots.
@@ -101,7 +100,6 @@ async def test_piper_backend():
   tiprack.print_grid()
 
   # Tube rack.
-  print("Get tube")
   tube_rack = lh.get_resource("Tube Rack [5x16]")
   _ = tube_rack["A1"]
   _ = tube_rack.make_grid()
@@ -117,11 +115,9 @@ async def test_piper_backend():
   _ = [tube.tracker.liquids for tube in tubes]
 
   # Aspirate.
-  print("Aspirate operation")
   # await lh.aspirate(tubes, vols=[100.0, 50.0, 200.0], use_channels=[0])
   await lh.aspirate(tubes[:1], vols=[100.0], use_channels=[0])
   # Dispense.
-  print("Dispense operation")
   # await lh.dispense(tubes, vols=[1.0, 1.0, 1.0])
   await lh.dispense(tubes[:1], vols=[10])
 
